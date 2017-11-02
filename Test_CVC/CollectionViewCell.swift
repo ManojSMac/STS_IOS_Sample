848ed9f643d107fe5ecc761fd1644ddcfc691cfa
Manoj <manoj.shenll@gmail.com>
2017-11-02 11:17:37 +0530

    Initial Source added

---
+++ Test_CVC/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Test_CVC
+//
+//  Created by STS_MACLAP on 28/08/17.
+//  Copyright © 2017 STS_MACLAP. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
